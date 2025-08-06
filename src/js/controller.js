9e538e68dac583ac19993d8f89e7c75890fc459b
TrungNguyen1811 <trung4atlam@gmail.com>
2025-08-06 14:54:40 +0700

    feat: implement search result - search food name then  show recipe list

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,20 @@
 import * as model from './model.js';
 import recipeView from './view/recipeView.js';
-
+import searchView from './view/searchView.js';
+import resultsView from './view/resultsView.js';
 import dataItemsJson from '../json/data-items.json';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -31,8 +34,24 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResult = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResult(query);
+
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    throw err;
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResult);
 };
 
 init();
